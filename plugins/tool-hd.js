3c9d8ae30a31750b72b5f2f57fa22a9bf4cd2937
VilewSja <111483311+VilewSja@users.noreply.github.com>
2022-09-01 18:13:15 +0800

    Update tool-hd.js

--- plugins/tool-hd.js
+++ plugins/tool-hd.js
@@ -5,7 +5,7 @@ deepai.setApiKey('31c3da72-e27e-474c-b2f4-a1b685722611')
 let handler = async (m) => {
 	let q = m.quoted ? m.quoted : m
 	let mime = (q.msg || q).mimetype || ''
-	if (!mime) throw 'ʀᴇᴘʟʏ ɢᴀᴍʙᴀʀɴʏᴀ ᴋᴀᴋ (～￣▽￣)～'
+	if (!mime) throw 'Reply Gambarnya'
     await m.reply(global.wait)
 	if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
 	let img = mime.split('/')[1]
@@ -14,7 +14,7 @@ let handler = async (m) => {
 	let resp = await deepai.callStandardApi('waifu2x', {
 		image: fs.readFileSync(`./${img}`)
 	})
-	await conn.sendFile(m.chat, resp.output_url, 'hd.jpg', 'ɪɴɪ ᴋᴀᴋ ʜᴀꜱɪʟɴʏᴀヾ(≧▽≦*)ᴏ', m).then(() => fs.unlinkSync(`./${img}`))
+	await conn.sendFile(m.chat, resp.output_url, 'hd.jpg', 'Hasilnya..', m).then(() => fs.unlinkSync(`./${img}`))
 }
 handler.help = ['hd <caption|reply media>']
 handler.tags = ['tools|anime']
