9529c50ed6df8e4c6ef27e6a9b5409bc11ac1997
VilewSja <111483311+VilewSja@users.noreply.github.com>
2022-09-02 10:30:21 +0000

    Update downloader-tiktok-audio.js

--- plugins/downloader-tiktok-audio.js
+++ plugins/downloader-tiktok-audio.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn, args }) => {
       }
     }
   })
-  let url = `https://api.zeeoneofc.xyz/api/downloader/tiktok?url=https://t.tiktok.com/i18n/share/video/7050032845158403354/?region=ID&mid=7028787667471010587&u_code=dkck2mgka0lmkh&preview_pb=0&language=id&_d=dm347b2b5m8daf&share_item_id=7050032845158403354&source=h5_t&timestamp=1641508306&user_id=7003032823783277595&sec_user_id=MS4wLjABAAAAQVcSMWZtoLQ3qqF4wHQOtvq8VoOQGmOor9X-gZgltrkCNbjbmOS-fjVR1gX0Dep9&utm_source=copy&utm_campaign=client_share&utm_medium=android&share_iid=7038903817816540955&share_link_id=0a8b0672-dcb7-494f-8659-102e11d39d1f&share_app_id=1180&apikey=liuuuOYJ&url=${args[0]}`
+  let url = `https://api.zeeoneofc.xyz&apikey=liuuuOYJ&url=${args[0]}`
   let txt = `🚀 *Link:* ${await (await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}`
   await conn.sendFile(m.chat, url, 'tiktokaudio.mp3', `
 
